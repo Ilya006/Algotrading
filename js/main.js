9b8ccbfd1df552bc1b46581f38a564bfdf8ebb00
ваше имя <Senognoevilya@gmail.com>
2020-09-23 12:56:09 +0500

    added form validation and mask for phone

--- js/main.js
+++ js/main.js
@@ -52,5 +52,69 @@ $(document).ready(function() {
       ]
     });
 
+    // модаотное окно
+    let modalBtn = document.querySelector('[data-toggle=modal]'),
+        modal = document.querySelector('.modal'),
+        modalDialog = document.querySelector('.modal__dialog'),
+        modalClose = document.querySelector('[data-toggle=modalClose]');
+
+    // открыть модальное окно
+    modalBtn.onclick = function() {
+      modal.classList.add('modal--visible');
+    };
+
+    // закрытие по кнопке
+    modalClose.onclick = function() {
+      modal.classList.remove('modal--visible');
+    };
+
+    // закрытие вне окна
+    modal.addEventListener('click', function(e) {
+      if(!modalDialog.contains(e.target)) {
+        modal.classList.remove('modal--visible')
+      }
+    });
+
+    // закрытие по esc
+    window.addEventListener('keypress', function(evt) {
+      if (window.event.keyCode) {
+        console.log(window.event.keyCode);
+        // modal.classList.remove('modal--visible')
+      }
+    });
+
+
+    // валидация формы
+    $('.modal__form').validate({
+      rules: {
+        userName: {
+          required: true,
+          minlength: 2
+        },
+        userPhone: 'required',
+        userEmail: {
+          required: true,
+          email: true
+        },
+        userPolicy: 'required'
+      },
+      messages: {
+        userName: {
+          required: 'Введите Имя',
+          minlength: 'Имя слишком короткое'
+        },
+        userPhone: 'Введите телефон',
+        userEmail: {
+          required: 'Ведите Email',
+          email: 'Формат: "..@domain.com"'
+        },
+        userPolicy: 'Подтвердите согласие на обработку данных'
+      }
+    });
+
+
+    // маска
+    $('#user-phone').mask('+7(000) 000-00-00')
+
 });
 
